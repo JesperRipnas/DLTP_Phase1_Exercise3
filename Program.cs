eb4dec252b065ead76e638f48962e58a731b7704
Rippan <jepperipnas@gmail.com>
2020-11-03 13:11:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace DLTP_Phase1_Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange fil att öppna: '\n");
+            string path = @"C:\Users\rippan\Desktop\file.txt"; //Console.ReadLine();
+            int rowCounter = 0;
+
+            var textFile = File.ReadLines(path);
+            foreach (var line in textFile)
+            {
+                Console.WriteLine(line);
+                rowCounter++;
+            }
+            Console.WriteLine($"\nRows: {rowCounter}");
+        }
+    }
+}
