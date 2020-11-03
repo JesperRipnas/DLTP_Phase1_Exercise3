60fca2b73f06f79c0b5f6d52021cefec21c7fd48
Rippan <jepperipnas@gmail.com>
2020-11-03 14:04:07 +0100

    added check for amount of words

--- Program.cs
+++ Program.cs
@@ -12,20 +12,26 @@ namespace DLTP_Phase1_Exercise3
             string path = Console.ReadLine();
             int rowCounter = 0;
             int charCounter = 0;
-
+            int wordCounter = 0;
+            //count rows
             var rows = File.ReadLines(path);
             foreach (var line in rows)
             {
                 Console.WriteLine(line);
                 rowCounter++;
             }
-
+            // count characters
             string text = File.ReadAllText(path);
             string textWithoutSpace = String.Concat(text.Where(c => !Char.IsWhiteSpace(c)));
             charCounter = textWithoutSpace.Length;
-
+            // count words
+            string[] words = text.Split(new[] { "\r\n", "\r", "\n", " " }, StringSplitOptions.None);
+            wordCounter = words.Length;
+            // Output
             Console.WriteLine($"\nRows: {rowCounter}");
+            Console.WriteLine($"Words: {wordCounter}");
             Console.WriteLine($"Characters: {charCounter}");
+
         }
     }
 }
